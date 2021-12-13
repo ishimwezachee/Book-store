ee7e8e5ca93875b106bf9dbbe665a1bc1ceb0aec
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-13 14:44:27 +0200

    fix linters

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../../components/button';
+import AddBooks from '../../components/addBook';
 
 const clickHandler = () => {
   alert('Remove Book');
@@ -8,6 +9,7 @@ const clickHandler = () => {
 const Book = () => (
   <div className="books">
     <h1>List of Books</h1>
+    <AddBooks />
     <Button click={clickHandler} value="Remove Book" />
   </div>
 );
