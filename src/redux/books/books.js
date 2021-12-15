5c3f29f632314c40e47fbd9301abf627f68bea8e
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-15 15:11:16 +0200

    create  add book to api action creator

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const FETCH_BOOK = 'FETCH_BOOK';
 const initialState = [];
 
 export const addBook = (payload) => ({
@@ -12,7 +13,28 @@ export const removeBook = (payload) => ({
   payload,
 });
 
-// post action creator
+// get book from backend
+export const getBook = (payload) => ({
+  type: FETCH_BOOK,
+  payload,
+});
+
+// Add book to backend
+export const addBookToApi = (payload) => async (dispatch) => {
+  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/DPo4nlYS7MkRGLDUZ9ls/books', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  dispatch(addBook(payload));
+};
+
+// // Add book to backend
+// export const addBookToApi =(payload) => async(dispatch)=>{
+//  const request = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/DPo4nlYS7MkRGLDUZ9ls/books');
+//  const response = await request.json();
+
+// }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
