711703c826dd253d3f56e67cc67a9bb7700b8634
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-17 10:58:30 +0200

    Add book list styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,49 @@
+// import { useDispatch } from 'react-redux';
+// import { v4 as uuidv4 } from 'uuid';
+// import { useState } from 'react';
+// import { addBookToApi } from '../redux/books/books';
+
+// const AddBooks = () => {
+//   const [value, setValue] = useState('');
+//   const [categoryValue, setCategory] = useState('');
+
+//   const dispatch = useDispatch();
+
+//   const submitBookToStore = (event) => {
+//     event.preventDefault();
+//     const newBook = {
+//       item_id: uuidv4(),
+//       title: value,
+//       category: categoryValue,
+//     };
+
+//     dispatch(addBookToApi(newBook));
+//     setValue('');
+//     setCategory('');
+//   };
+//   const categoryHandler = (e) => {
+//     setCategory(e.target.value);
+//   };
+//   const valueHandler = (e) => {
+//     setValue(e.target.value);
+//   };
+//   return (
+//     <form onSubmit={submitBookToStore}>
+//       <input value={value} onChange={valueHandler} placeholder="ADD NEW BOOK" required />
+//       <select name="books" id="book" value={categoryValue} onChange={categoryHandler} required>
+//         <option value="">Select</option>
+//         <option value="Action">Action</option>
+//         <option value="Science fiction">Science fiction</option>
+//         <option value="Economy">Economy</option>
+//         <option value="Computer Science">Computer Science</option>
+//       </select>
+//       <input type="submit" value="Add Book" />
+//     </form>
+//   );
+// };
+
+// export default AddBooks;
+
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
@@ -9,8 +55,8 @@ const AddBooks = () => {
 
   const dispatch = useDispatch();
 
-  const submitBookToStore = (event) => {
-    event.preventDefault();
+  const submitBookToStore = (e) => {
+    e.preventDefault();
     const newBook = {
       item_id: uuidv4(),
       title: value,
@@ -24,21 +70,25 @@ const AddBooks = () => {
   const categoryHandler = (e) => {
     setCategory(e.target.value);
   };
-  const valueHandler = (e) => {
-    setValue(e.target.value);
-  };
   return (
-    <form onSubmit={submitBookToStore}>
-      <input value={value} onChange={valueHandler} placeholder="ADD NEW BOOK" required />
-      <select name="books" id="book" value={categoryValue} onChange={categoryHandler} required>
-        <option value="">Select</option>
-        <option value="Action">Action</option>
-        <option value="Science fiction">Science fiction</option>
-        <option value="Economy">Economy</option>
-        <option value="Computer Science">Computer Science</option>
-      </select>
-      <input type="submit" value="Add Book" />
-    </form>
+    <div className="form-div">
+      <hr />
+      <h3 className="new-book">ADD NEW BOOK</h3>
+      <form>
+        <input className="inp" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Book title" required />
+        <select className="cate" name="books" id="book" onChange={categoryHandler} required>
+          <option>Category</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+          <option value="Cooking">Cooking</option>
+          <option value="Non-Fiction">Non-Fiction</option>
+          <option value="History">History</option>
+          <option value="Romance">Romance</option>
+        </select>
+        <input className="adding" type="button" value="ADD BOOK" onClick={submitBookToStore} />
+      </form>
+    </div>
   );
 };
 
